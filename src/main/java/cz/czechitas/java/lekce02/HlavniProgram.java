5580fe13869cee9b0f03494c3cfda521401491fa
Filip Jirsák <filip@jirsak.org>
2023-02-13 22:46:03 +0100

    Metoda start() je veřejná.

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -4,14 +4,33 @@ import cz.czechitas.java.lekce02.engine.Turtle;
 
 public class HlavniProgram {
     private Turtle zofka;
-    private void start() {
+
+    public void start() {
         zofka = new Turtle();
 
+
         //TODO tady bude kód
+        for (int i = 0; i < 4; i++) {
+            zofka.move(100);
+            zofka.turnRight(90);
+        }
+
+            zofka.move(100);
+            zofka.turnRight(45);
+
+            zofka.move(100);
+            zofka.turnRight(90);
+
+            zofka.move(100);
+            zofka.turnLeft(15);
+
+
+
     }
 
-    public static void main(String[] args) {
-        new HlavniProgram().start();
+        public static void main (String[]args) {
+            new HlavniProgram().start();
+        }
+
     }
 
-}
