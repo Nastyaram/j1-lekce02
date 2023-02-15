5dbbc263cd6b5c55164fde582f29f2e3f81db224
nastyaram <a.ramodina@gmail.com>
2023-02-15 16:52:36 +0100

    čtverec

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -6,12 +6,24 @@ public class HlavniProgram {
     private Turtle zofka;
     public void start() {
         zofka = new Turtle();
+
+        nakresliCtverec();
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(200);
+        zofka.turnRight(90);
+        zofka.penDown();
+
+        nakresliCtverec();
+
+        //TODO tady bude kód
+    }
+
+    private void nakresliCtverec() {
         for (int i = 0; i < 4; i++) {
             zofka.move(100);
             zofka.turnRight(90);
         }
-
-        //TODO tady bude kód
     }
 
     public static void main(String[] args) {
