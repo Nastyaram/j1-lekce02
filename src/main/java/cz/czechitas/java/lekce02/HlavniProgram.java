80922264d93c4cd8a7811983d69203a3019c37e1
nastyaram <a.ramodina@gmail.com>
2023-02-15 16:33:40 +0100

    čtverec

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -4,8 +4,12 @@ import cz.czechitas.java.lekce02.engine.Turtle;
 
 public class HlavniProgram {
     private Turtle zofka;
-    private void start() {
+    public void start() {
         zofka = new Turtle();
+        for (int i = 0; i < 4; i++) {
+            zofka.move(100);
+            zofka.turnRight(90);
+        }
 
         //TODO tady bude kód
     }
